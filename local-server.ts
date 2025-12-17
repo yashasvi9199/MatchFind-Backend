2e9996927773f241e56cac32d60f74276095e2dc
Yash Haldiya <yashasvi9199@gmail.com>
2025-12-17 16:53:45 +0530

    FIX: Change default port to 3001 to avoid frontend conflict

--- local-server.ts
+++ local-server.ts
@@ -1,6 +1,6 @@
 import app from './api/index';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
